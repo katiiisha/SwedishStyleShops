9a27a54fa6bf8396f2319208a697e6dbfabfd6ca
Kate Mendeleeva <mendeleevakate2002@gmail.com>
2024-06-16 03:47:17 +0300

    dk;dks

--- js/feedback.js
+++ js/feedback.js
@@ -1,9 +1,10 @@
 const modalFeedback = document.querySelector('.modal-feedback');
 const btnFeedback = document.querySelector('.feedback-btn');
+console.log(btnFeedback);
 const btnFeedbackClose = document.querySelector('.feedback-form_close');
 const messageFeedback = document.querySelector('.message-feedback');
 const feedbackForm = document.querySelector('.feedback-form');
-console.log(modalFeedback, btnFeedback);
+
 const closemessageFeedback = function () {
   messageFeedback.classList.remove('message-feedback__active');
 };
@@ -11,11 +12,9 @@ const closemessageFeedback = function () {
 btnFeedback.addEventListener('click', () => {
   modalFeedback.classList.add('modal-feedback__active');
   modalFeedback.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  bodyScrollLock.disableBodyScroll(modalFeedback);
 });
 btnFeedbackClose.addEventListener('click', () => {
   modalFeedback.classList.remove('modal-feedback__active');
-  bodyScrollLock.enableBodyScroll(modalFeedback);
 });
 
 feedbackForm.addEventListener('submit', (e) => {
@@ -23,6 +22,5 @@ feedbackForm.addEventListener('submit', (e) => {
   modalFeedback.classList.remove('modal-feedback__active');
   messageFeedback.classList.add('message-feedback__active');
   messageFeedback.scrollIntoView({ block: 'start', behavior: 'smooth' });
-  bodyScrollLock.enableBodyScroll(modalFeedback);
   setTimeout(closemessageFeedback, 3000);
 });
